4708acd1116111e3a18315536ff562353e7129fb
emrekaandrms <54370499+emrekaandrms@users.noreply.github.com>
2020-03-10 16:27:10 +0300

    Updates

---
+++ precache-manifest.60dba4362cb1f7ecdd2cd6a6eb61586c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ea990eec5a96c95c4d75f23cb0bcda",
+    "url": "/fb-react-project/index.html"
+  },
+  {
+    "revision": "7f33b4bae18b11d9e82d",
+    "url": "/fb-react-project/static/css/main.e7cccb22.chunk.css"
+  },
+  {
+    "revision": "b57bf7f784ed396f64f9",
+    "url": "/fb-react-project/static/js/2.9d724df1.chunk.js"
+  },
+  {
+    "revision": "7f33b4bae18b11d9e82d",
+    "url": "/fb-react-project/static/js/main.aaca70f4.chunk.js"
+  },
+  {
+    "revision": "7170b88bdb8d7cf33b27",
+    "url": "/fb-react-project/static/js/runtime~main.7d46428c.js"
+  }
+]);
